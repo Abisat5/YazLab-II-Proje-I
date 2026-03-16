b9a48cf986985c24f0388f3bbddddd10f9d36890
Talha Fırat <tfmesebusiness@gmail.com>
2026-03-16 18:21:33 +0300

    Dispatcher loggingFilter icin TDD Red asamasi eklendi

--- dispatcher/src/test/java/com/yazlab/dispatcher/DispatcherApplicationTests.java
+++ dispatcher/src/test/java/com/yazlab/dispatcher/DispatcherApplicationTests.java
@@ -1,22 +1,20 @@
 package com.yazlab.dispatcher;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 class DispatcherApplicationTests {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private ApplicationContext context;
 
     @Test
-    void testDispatcherUp() {
-        ResponseEntity<String> response = restTemplate.getForEntity("/test", String.class);
-        assertEquals("Dispatcher calisiyor sikinti yok", response.getBody());
+    void testLoggingFilterExists() {
+        assertThat(context.containsBean("loggingFilter")).isTrue();
     }
 }
