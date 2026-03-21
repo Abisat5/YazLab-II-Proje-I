0e072ebbf33e80eb1ea0864a97f64717d27d2ea3
Talha Fırat <148004819+tfmese@users.noreply.github.com>
2026-03-21 22:54:40 +0300

    HTTP 401 unauthorized response için fail test eklendi.

--- dispatcher/src/test/java/com/yazlab/dispatcher/DispatcherApplicationTests.java
+++ dispatcher/src/test/java/com/yazlab/dispatcher/DispatcherApplicationTests.java
@@ -4,17 +4,32 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DispatcherApplicationTests {
 
     @Autowired
     private ApplicationContext context;
 
+    @Autowired
+    private MockMvc mockMvc; // API isteklerini simüle etmek için
+
     @Test
     void testLoggingFilterExists() {
         assertThat(context.containsBean("loggingFilter")).isTrue();
+    }
+
+    // TDD kuralı gereği asıl kodu düzeltmeden önce yazdığımız test
+    @Test
+    void tokenExceptionMustBe401() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isUnauthorized());
     }
 }
