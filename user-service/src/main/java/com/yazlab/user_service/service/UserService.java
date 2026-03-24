35b92b5af943b369c1380e11121adcbbc8fe202d
Talha Fırat <tfmesebusiness@gmail.com>
2026-03-24 18:25:45 +0300

    mikroservis kurulumu ve iletişim testleri
    - Projedeki 3 ana servis Auth, User, Dispatcher çalışır halde.
    - Docker'daki MongoDB ile servislerin bağlantısını kruuldu.
    - Servislerin birbiriyle konuşup konuşmadığını Dispatcher üzerinden test ettim.
    - Register ve kullanıcı listeleme kısımları ufak pürüzlerle çalışıyor.

--- user-service/src/main/java/com/yazlab/user_service/service/UserService.java
+++ user-service/src/main/java/com/yazlab/user_service/service/UserService.java
@@ -1,23 +1,21 @@
 package com.yazlab.user_service.service;
 
 import com.yazlab.user_service.model.User;
+import com.yazlab.user_service.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserService {
 
-    private final List<User> users = new ArrayList<>();
+    private final UserRepository userRepository;
 
-    public UserService() {
-        // Mock kullanıcı ekle
-        users.add(new User("alp", "1234"));
-        users.add(new User("ayse", "abcd"));
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     public List<User> getAllUsers() {
-        return users;
+        return userRepository.findAll();
     }
 }
