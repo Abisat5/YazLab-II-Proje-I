95f024a5291642ffa663a8a0ec1b738b971d3a9e
Alperen KESKİN <75186682+Abisat5@users.noreply.github.com>
2026-04-02 17:22:11 +0300

    (GREEN) İsterlere Bağlı olarak İkinci TEST Doğrulamaları --> /access-rules-404

--- dispatcher/src/main/java/com/yazlab/dispatcher/controller/AccessRuleController.java
+++ dispatcher/src/main/java/com/yazlab/dispatcher/controller/AccessRuleController.java
@@ -30,7 +30,9 @@ public class AccessRuleController {
 
     @DeleteMapping("/access-rules/{id}")
     public ResponseEntity<Void> deleteAccessRule(@PathVariable String id) {
-        
+        if (!accessRuleRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
         accessRuleRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
